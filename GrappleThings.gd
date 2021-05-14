42dc7d854a80a02d60c60066defc728601072164
Calvin Wong <ctwong958@berkeley.edu>
2021-05-14 08:59:17 -0700

    Make skeleton

--- GrappleThings.gd
+++
@@ -1,5 +0,0 @@
-extends Spatial
-
-func _ready():
-	for box in get_children():
-		box.rotation = Vector3(3, 3, 3) * randf()
--- player/player.gd
+++ player/player.gd
@@ -3,10 +3,6 @@ extends KinematicBody
 
 # Convenience Nodes
 onready var cam_helper := $CamHelper
-onready var hook := $CamHelper/Hook
-onready var line_helper := $LineHelper
-onready var line := $LineHelper/Line
-export var grapple_point : NodePath 
 
 # Player Controller
 export var MOUSE_SENSITIVITY := .001
@@ -16,19 +12,6 @@ export var friction := .25  # Higher -> more friction
 export var jump_strength := 32.0
 var velocity := Vector3()
 
-# Grappling
-export var max_grapple_speed := 2.75 # Self explanatory
-export var grapple_speed := .5
-""" Also known as the spring constant, this is how stiff your rope is. 
-	For this demo, doesn't actually do too much, but you can play with
-	the numbers
-"""
-export var rest_length := 1.0
-"""How far the player should rest from the grapple point"""
-var hooked := false
-var grapple_position := Vector3()
-
-
 func _ready() -> void:
 	# Capture mouse at start
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -36,68 +19,10 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	move(delta)
 	collide_with_rigidbodies()
-	handle_hook()
 	if Input.is_action_pressed("slowmo"):
 		Engine.time_scale = .1
 	else:
 		Engine.time_scale = 1
-
-
-# HOOK STUFF ---------------------------------------------------------
-
-func look_for_point() -> void:
-	var grapple_pt := get_node_or_null(grapple_point)
-	if grapple_pt and hook.is_colliding():
-		grapple_pt.translation = hook.get_collision_point()
-
-func check_hook_activation() -> void:
-	# Activate hook
-	if Input.is_action_just_pressed("hook") and hook.is_colliding():
-		hooked = true
-		grapple_position = hook.get_collision_point()
-		line.show()
-	
-	# Stop grappling
-	elif Input.is_action_just_released("hook"):
-		hooked = false
-		line.hide()
-
-func handle_hook() -> void:
-	check_hook_activation()
-	var length := calculate_path()
-	draw_hook(length)
-	look_for_point()
-
-# Adds to player velocity and returns the length of the hook rope
-func calculate_path() -> float:
-	var player2hook := grapple_position - translation # vector from player to hook
-	var length := player2hook.length()
-	if hooked:
-		# if we more than 4 away from line, don't dampen speed as much
-		if length > 4:
-			velocity *= .999
-		# Otherwise dampen speed more
-		else:
-			velocity *= .9
-		
-		# Hook's law equation
-		var force := grapple_speed * (length - rest_length)
-		
-		# Clamp force to be less than max_grapple_speed
-		if abs(force) > max_grapple_speed:
-			force = max_grapple_speed
-		
-		# Preserve direction, but scale by force
-		velocity += player2hook.normalized() * force
-	
-	return length
-
-# Makes the line have length LENGTH
-func draw_hook(length: float) -> void:
-	line_helper.look_at(grapple_position, Vector3.UP)
-	line.height = length
-	line.translation.z = length / -2
-
 
 # CHARACTER CONTROLLER -------------------------------------------------
 
@@ -123,8 +48,7 @@ func move(delta: float) -> void:
 		velocity += input * air_speed
 	
 	# Gravity
-	if !hooked:
-		velocity += Vector3.DOWN
+	velocity += Vector3.DOWN
 	
 	# Move player using velocity, we want to have the UP vector as our up,
 	# the false at the end allows us to have better collisions
